da53c90130804e13b909f86580bec4ae5ef1381c
Johnson Earls <johnson.earls@gmail.com>
2016-09-08 10:02:01 -0700

    Fix test file for gpgfile

--- tests/init.pp
+++ tests/init.pp
@@ -9,4 +9,8 @@
 # Learn more about module testing here:
 # http://docs.puppetlabs.com/guides/tests_smoke.html
 #
-include gpgfile
+gpgfile { 'test':
+  ensure => decrypted ,
+  content => 'content' ,
+  noop => true ,
+}
